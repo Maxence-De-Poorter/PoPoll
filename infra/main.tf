7b90b0222c9eefd31863e4d982933f9fe22fba2a
Maxence DE POORTER <github.flo1u@passmail.net>
2025-12-12 15:05:15 +0100

    fix: Ajout de Azure fonction + séparation CI/CD

--- infra/main.tf
+++ infra/main.tf
@@ -13,7 +13,6 @@ resource "azurerm_resource_group" "rg" {
   location = var.location_swa
 }
 
-# Cosmos DB account
 resource "azurerm_cosmosdb_account" "cosmos" {
   name                = "cosmos-${local.name}"
   location            = var.location_data
@@ -23,7 +22,9 @@ resource "azurerm_cosmosdb_account" "cosmos" {
 
   capabilities { name = "EnableServerless" }
 
-  consistency_policy { consistency_level = "Session" }
+  consistency_policy {
+    consistency_level = "Session"
+  }
 
   geo_location {
     location          = var.location_data
@@ -31,19 +32,54 @@ resource "azurerm_cosmosdb_account" "cosmos" {
   }
 }
 
-# Static Web App
-resource "azurerm_static_web_app" "swa" {
-  name                = "swa-${local.name}"
-  location            = var.location_swa
+resource "azurerm_storage_account" "funcsa" {
+  name                     = "funcsa${random_string.suffix.result}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = var.location_data
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_service_plan" "funcplan" {
+  name                = "plan-func-${local.name}"
   resource_group_name = azurerm_resource_group.rg.name
-  sku_tier            = "Free"
-  sku_size            = "Free"
+  location            = var.location_data
+  os_type             = "Linux"
+  sku_name            = "Y1"
+}
+
+resource "azurerm_linux_function_app" "api" {
+  name                       = "func-${local.name}"
+  resource_group_name        = azurerm_resource_group.rg.name
+  location                   = var.location_data
+  service_plan_id            = azurerm_service_plan.funcplan.id
+  storage_account_name       = azurerm_storage_account.funcsa.name
+  storage_account_access_key = azurerm_storage_account.funcsa.primary_access_key
+
+  site_config {
+    application_stack {
+      node_version = "20"
+    }
+  }
 
   app_settings = {
-    COSMOS_CONNECTION_STRING  = azurerm_cosmosdb_account.cosmos.primary_sql_connection_string
-    COSMOS_DB_NAME            = "strawpoll"
-    COSMOS_CONTAINER_NAME     = "polls"
-    COSMOS_PARTITION_KEY_PATH = "/id"
-    ALLOWED_ORIGIN            = "http://localhost:5173"
+    FUNCTIONS_WORKER_RUNTIME = "node"
+    WEBSITE_RUN_FROM_PACKAGE = "1"
+
+    COSMOS_CONNECTION_STRING = azurerm_cosmosdb_account.cosmos.primary_sql_connection_string
+    COSMOS_DB_NAME           = "strawpoll"
+    COSMOS_CONTAINER_NAME    = "polls"
+
+    ALLOWED_ORIGIN = "http://localhost:5173,https://${azurerm_static_web_app.swa.default_host_name}"
   }
+
+  depends_on = [azurerm_static_web_app.swa]
+}
+
+resource "azurerm_static_web_app" "swa" {
+  name                = "swa-${local.name}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location_swa
+  sku_tier            = "Free"
+  sku_size            = "Free"
 }
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -6,8 +6,15 @@ output "static_web_app_default_host_name" {
   value = azurerm_static_web_app.swa.default_host_name
 }
 
-# Important: token de déploiement à mettre dans GitHub Secrets
 output "static_web_app_api_key" {
   value     = azurerm_static_web_app.swa.api_key
   sensitive = true
+}
+
+output "function_app_name" {
+  value = azurerm_linux_function_app.api.name
+}
+
+output "function_app_url" {
+  value = "https://${azurerm_linux_function_app.api.default_hostname}"
 }
